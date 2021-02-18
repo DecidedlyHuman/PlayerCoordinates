ab937ad46e342b727071c41a20d8595883499235
DecidedlyHuman <69521518+DecidedlyHuman@users.noreply.github.com>
2021-02-18 19:00:12 +0000

    Initial commit.

---
+++ PlayerCoordinates/Globals.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace PlayerCoordinates
+{
+    public struct Coordinates
+    {
+        public int x, y;
+
+        public Coordinates(int newX, int newY)
+        {
+            x = newX;
+            y = newY;
+        }
+
+        public static implicit operator Coordinates(Vector2 v)
+        {
+            return new Coordinates((int)v.X, (int)v.Y);
+        }
+    }
+}
---
+++ PlayerCoordinates/ModConfig.cs
@@ -0,0 +1,11 @@
+﻿using StardewModdingAPI;
+
+namespace PlayerCoordinates
+{
+    public class ModConfig
+    {
+        public SButton CoordinateHUDToggle  { get; } = SButton.F8;
+        public SButton LogCoordinates { get; set; } = SButton.F9;
+        public SButton SwitchToCursorCoords { get; set; } = SButton.F10;
+    }
+}
